c5f547697cce44820869f969110df44796d2e2a5
jgaffuri <jgaf@proton.me>
2024-09-27 15:50:45 +0200

    atlas

--- src/atlas.py
+++ src/atlas.py
@@ -12,7 +12,6 @@ make_svg_map(
     '/home/juju/geodata/census/Eurostat_Census-GRID_2021_V2-0/ESTAT_Census_2021_V2.csv',
     1000,
     scale = 1/4500000,
-    #width_mm = 841, height_mm = 1189, #A0
     width_mm = 210, height_mm = 297, #A4
     cx = 4300000, cy = 3300000,
     )
