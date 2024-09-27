a38903bdf107c02b45b7cb8694a016069b770735
jgaffuri <jgaf@proton.me>
2024-09-27 14:46:21 +0200

    atlas

--- src/make_svg_old.py
+++ src/make_svg_old.py
@@ -4,16 +4,42 @@ import fiona
 from trivariate import trivariate_classifier
 
 
+out_folder = '/home/juju/gisco/census_2021_map/'
 
-def make_svg_map(
-        out_svg_path,
-        in_CSV,
-        res = 1000,
-        scale = 1/4500000,
-        width_mm = 841, height_mm = 1189,
-        cx = 4300000, cy = 3300000,
-        colors = {"0": "#4daf4a", "1": "#377eb8", "2": "#e41a1c", "m0": "#ab606a", "m1": "#ae7f30", "m2": "#4f9685", "center": "#999"}
-        ):
+#the grid resolution in meters
+res = 5000
+in_CSV = '/home/juju/geodata/census/out/ESTAT_Census_2021_V2_'+str(res)+'.csv'
+
+#the maximum population threshold - depends on the resolution
+max_pop = res * 60
+
+#the visualisation scale
+scale = 1/4500000
+
+#style parameters
+
+#minimum circle size: 0.25 mm
+min_diameter = 0.25 / 1000 / scale
+#maximum diameter: 1.6*resolution
+max_diameter = res * 1.6
+#print(min_diameter, max_diameter)
+power = 0.25
+
+#define the trivariate classifier
+classifier = trivariate_classifier(
+    ['Y_LT15', 'Y_1564', 'Y_GE65'],
+    lambda cell:cell["T_"],
+    {'center': [0.15, 0.6, 0.25], 'centerCoefficient': 0.25}
+    )
+
+#define the colors, for each trivariate class
+colors = {"0": "#4daf4a", "1": "#377eb8", "2": "#e41a1c", "m0": "#ab606a", "m1": "#ae7f30", "m2": "#4f9685", "center": "#999"}
+
+
+def make_map(path_svg,
+             width_mm = 841, height_mm = 1189,
+             cx = 4300000, cy = 3300000
+             ):
 
     # transform for europe view
     # A0 dimensions in millimeters
@@ -24,7 +50,7 @@ def make_svg_map(
     transform_str = f"scale({scale*1000*96/25.4} {scale*1000*96/25.4}) translate({-x_min} {-y_min})"
 
     # Create an SVG drawing object with A0 dimensions in landscape orientation
-    dwg = svgwrite.Drawing(out_svg_path, size=(f'{width_mm}mm', f'{height_mm}mm'))
+    dwg = svgwrite.Drawing(path_svg, size=(f'{width_mm}mm', f'{height_mm}mm'))
     # Set the viewBox attribute to map the custom coordinates to the SVG canvas
     #dwg.viewbox(x_min, y_min, width_m, height_m)
     #dwg.viewbox(0, 0, width_mm/1000*96/25.4, height_mm/1000*96/25.4)
@@ -73,27 +99,6 @@ def make_svg_map(
     #dwg.add(dwg.rect(insert=(x_min, y_min), size=(width_m, height_m), fill='#dfdfdf'))
 
 
-    #the maximum population threshold - depends on the resolution
-    max_pop = res * 60
-
-    #style parameters
-
-    #minimum circle size: 0.25 mm
-    min_diameter = 0.25 / 1000 / scale
-    #maximum diameter: 1.6*resolution
-    max_diameter = res * 1.6
-    #print(min_diameter, max_diameter)
-    power = 0.25
-
-
-    #define the trivariate classifier
-    classifier = trivariate_classifier(
-        ['Y_LT15', 'Y_1564', 'Y_GE65'],
-        lambda cell:cell["T_"],
-        {'center': [0.15, 0.6, 0.25], 'centerCoefficient': 0.25}
-        )
-
-
     #print("Draw cells")
     gCircles = dwg.g(id='circles', transform=transform_str)
     for cell in cells:
@@ -139,3 +144,15 @@ def make_svg_map(
     dwg.save()
 
 
+
+print("Make europe map")
+make_map(out_folder + 'map_age_EUR.svg')
+
+print("Make CY map")
+make_map(path_svg = out_folder + 'map_age_CY.svg', width_mm = 50, height_mm = 40, cx = 6438000, cy = 1678693)
+print("Make Canaries map")
+make_map(path_svg = out_folder + 'map_age_cana.svg', width_mm = 120, height_mm = 60, cx = 1805783, cy = 1020991)
+print("Make Madeira map")
+make_map(path_svg = out_folder + 'map_age_madeira.svg', width_mm = 30, height_mm = 15, cx = 1841039, cy = 1522346)
+print("Make Azores map")
+make_map(path_svg = out_folder + 'map_age_azor.svg', width_mm = 110, height_mm = 140, cx = 1140466, cy = 2505249)
