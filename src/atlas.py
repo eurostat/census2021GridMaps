4b11186954565716bcb4ea96ff3e4dff8e830157
jgaffuri <jgaf@proton.me>
2024-10-08 16:54:32 +0200

    atlas

--- src/atlas.py
+++ src/atlas.py
@@ -81,6 +81,6 @@ def combine_pdf_pages():
     print("combine", len(pdfs), "pages")
     combine_pdfs(pdfs, out_folder + "atlas.pdf")
 
-make_svg_pages()
-make_pdf_pages()
+#make_svg_pages()
+make_pdf_pages(True)
 combine_pdf_pages()
